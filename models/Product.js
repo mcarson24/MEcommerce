18e86eccc697b1a76bbf00ffecf880dcc2b274c7
Matthew Carson <matthew.carson@pm.me>
2022-04-21 01:28:20 -0400

    Fix Product Initialization

--- models/Product.js
+++ models/Product.js
@@ -28,13 +28,13 @@ Product.init(
         defaultValue: 10,
         validate: {
           isNumeric: true
-        },
-        category_id: {
-          type: DataTypes.INTEGER,
-          references: {
-            model: 'category',
-            key: 'id'
-          }
+        }
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id'
         }
       }
     }
